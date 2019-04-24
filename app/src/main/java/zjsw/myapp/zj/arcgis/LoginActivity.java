4b8deb65c80ffc415f38d6b16689ce8dba1991cc
Wangxueyang1988 <657013794@qq.com>
2019-04-24 10:54:17 +0800

    测试1

--- app/src/main/java/zjsw/myapp/zj/arcgis/LoginActivity.java
+++ app/src/main/java/zjsw/myapp/zj/arcgis/LoginActivity.java
@@ -81,7 +81,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
